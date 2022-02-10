efa145882ebaab4e682fa386f6f51dfe43b1f622
alext <alextyrer@btinternet.com>
2022-02-10 08:04:29 +0000

    Start of move to number hinting

--- Solver.py
+++ Solver.py
@@ -6,6 +6,8 @@ import pygame
 class Solver:
     def __init__(self, screen):
         self.board = [[0 for _ in range(9)] for _ in range(9)]
+        self.hints = [[[i for i in range(1, 10)] for _ in range(9)] for _ in range(9)]  # num hints
+
         # self.board = [i*9+j]
         self.scr_w = screen.get_width()
         self.scr_h = screen.get_height()
@@ -59,8 +61,20 @@ class Solver:
                 self.board = eval(input())  # defo really stupid
             if pressed_keys[pygame.K_s]:
                 self.solving = True
+                #print(self.board)
+                #print(self.hints)
+                self.flush()
+                #print(self.board)
+                #print(self.hints)
+                #print()
             if pressed_keys[pygame.K_t]:
                 self.ticking = not self.ticking
+
+    def flush(self):
+        for i in range(9):
+            for j in range(9):
+                if self.board[i][j] != 0:
+                    self.hints[i][j] = []
 
     def solve(self, tick):
         def crossout(num):
@@ -68,35 +82,36 @@ class Solver:
                 for j in range(9):
                     if self.board[i][j] == num:
                         for k in range(9):
-                            if self.board[i][k] == 0:
-                                self.board[i][k] = -1
-                            if self.board[k][j] == 0:
-                                self.board[k][j] = -1
-                            if self.board[i // 3 * 3 + k // 3][j // 3 * 3 + k % 3] == 0:
-                                self.board[i // 3 * 3 + k // 3][j // 3 * 3 + k % 3] = -1
+                            if num in self.hints[i][k] and j != k:
+                                self.hints[i][k].remove(num)
+                            if num in self.hints[k][j] and i != k:
+                                self.hints[k][j].remove(num)
+                            #if num in self.hints[i // 3 * 3 + k // 3][j // 3 * 3 + k % 3]:
+                             #   self.hints[i // 3 * 3 + k // 3][j // 3 * 3 + k % 3].remove(num)
 
         def fill(num):
             for i in range(9):
                 for j in range(9):
-                    if self.board[i][j] == 0:
+                    if len(self.hints[i][j]) == 1:
+                        self.board[i][j] = self.hints[i][j][0]
+                        self.hints[i][j] = []
+                    if self.board[i][j] == 0 and num in self.hints[i][j]:
                         filled = [0, 0, 0]
                         for k in range(9):
-                            if self.board[i][k] in (0, num):
+                            if num in self.hints[i][k] or self.board[i][k] == num:
                                 filled[0] += 1
-                            if self.board[k][j] in (0, num):
+                            if num in self.hints[k][j] or self.board[k][j] == num:
                                 filled[1] += 1
-                            if self.board[i // 3 * 3 + k // 3][j // 3 * 3 + k % 3] in (0, num):
+                            if num in self.hints[i // 3 * 3 + k // 3][j // 3 * 3 + k % 3] or\
+                                    self.board[i // 3 * 3 + k // 3][j // 3 * 3 + k % 3] == num:
                                 filled[2] += 1
                         if min(filled) == 1:
                             self.board[i][j] = num
-                            print(filled)
+                            self.hints[i][j] = []
+                            # print(filled)
                         # print(i, j, min(filled))
 
-        def restore():
-            for i in range(9):
-                for j in range(9):
-                    if self.board[i][j] == -1:
-                        self.board[i][j] = 0
+
 
         def complete():
             for i in range(9):
@@ -113,8 +128,9 @@ class Solver:
         elif tick % 30 == 10:
             fill(int((tick - 10) % 270 / 30+1))
         elif tick % 30 == 20:
-            restore()
             if complete():
                 self.solving = False
 
 
+# TODO: gets stuck on second sudoku and idk why
+# TODO: After testing I have determined that it cannot tell when there is only one possibility in the row so fix this
